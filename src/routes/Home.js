f348d3f73844375b28f48f9390a23bb85af302ca
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-04-01 14:43:43 +0900

    #3.2 Getting the Qweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [qweet, setQweet] = useState("");
+  const [qweets, setQweets] = useState([]);
+
+  const getQweets = async () => {
+    const dbQweets = await dbService.collection("qweets").get();
+    dbQweets.forEach((document) => {
+      const qweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setQweets((prev) => [qweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getQweets();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -20,6 +36,8 @@ const Home = () => {
     setQweet(value);
   };
 
+  console.log(qweets);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -31,6 +49,13 @@ const Home = () => {
         />
         <input type="submit" value="Qweet" />
       </form>
+      <div>
+        {qweets.map((qweet) => (
+          <div key={qweet.id}>
+            <h4>{qweet.qweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

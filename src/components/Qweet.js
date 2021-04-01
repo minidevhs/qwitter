26749308da10a1ccb27ff23d616bce8bd3be146d
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-04-01 18:55:31 +0900

    #3.5 Delete and Update part Two

--- src/components/Qweet.js
+++ src/components/Qweet.js
@@ -1,15 +1,65 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
-const Qweet = ({ qweetObj, isOwner }) => (
-  <div>
-    <h4>{qweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete Qweet</button>
-        <button>Edit Qweet</button>
-      </>
-    )}
-  </div>
-);
+const Qweet = ({ qweetObj, isOwner }) => {
+  const [edit, setEdit] = useState(false);
+  const [newQweet, setNewQweet] = useState(qweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this qweet?");
+    if (ok) {
+      await dbService.doc(`qweets/${qweetObj.id}`).delete();
+    }
+  };
+
+  const toggleEditting = () => {
+    setEdit((prev) => !prev);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.doc(`qweets/${qweetObj.id}`).update({
+      text: newQweet,
+    });
+    setEdit(false);
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewQweet(value);
+  };
+
+  return (
+    <div>
+      {edit ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              onChange={onChange}
+              type="text"
+              placeholder="Edit your qweet"
+              value={newQweet}
+              required
+            />
+            <input type="submit" value="Update Qweet" />
+          </form>
+          <button onClick={toggleEditting}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{qweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Qweet</button>
+              <button onClick={toggleEditting}>Edit Qweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Qweet;

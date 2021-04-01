afb648f2017ee7c235b458a45ea85cbc615cfbfe
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-04-01 14:17:13 +0900

    #3.0 Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [qweet, setQweet] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setQweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type={qweet}
+          onChange={onChange}
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Qweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
